3bfc593bf83985b13306d942caef7de3dbebdfd0
VelocityPulse <velocitypulse@outlook.fr>
2022-06-03 11:06:03 +0200

    Move project to double project python/kotlin

---
+++ IndicielCyrptoMarket/src/main/kotlin/FetchData.kt
@@ -0,0 +1,19 @@
+class FetchData {
+
+    private val dataPath = "./data/"
+    private val symbolsPath = "symbols/"
+    private val currencyList = "currency_list"
+    private val stableCoins =
+        arrayListOf("busd", "usdt", "usdc", "dai", "tusd", "usdp", "usdn", "usdd", "fei")
+
+    private val additionalCoins =
+        arrayListOf("terra_luna")
+
+
+    public fun fetchData() {
+        println("Fetch data")
+
+
+    }
+
+}
---
+++ IndicielCyrptoMarket/src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+
+    if (args.size > 0) {
+        if ("fetch_data" == args[0])
+            FetchData().apply {
+                this.fetchData()
+//                this.calculatePositions()
+            }
+        else if ("simulation" == args[0])
+            println("use python")
+    }
+
+}
