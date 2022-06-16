b65a30dbd354cafc128d91e16ca9b2aeb71e709e
VelocityPulse <velocitypulse@outlook.fr>
2022-06-16 20:48:03 +0200

    [PYTHON] WIP - Initiate first day and be ready for backtest process

--- PythonSimulation/backTestSimulation.py
+++ PythonSimulation/backTestSimulation.py
@@ -30,9 +30,10 @@ class BackTestSimulation:
     def check_wallet_percent_is_equal_to_100(self, daily_wallet_collection):
         total_percent = 0
         for crypto in daily_wallet_collection.items():
-            percent = crypto[1][-1]
+            percent = crypto[1]["percent_importance"]
             if percent > 100 or percent < 0:
-               percent
+                percent
+                assert False
             total_percent += percent
 
         assert total_percent == 100
@@ -46,7 +47,7 @@ class BackTestSimulation:
         # ending_day = 3312
         ending_day = 3230
         max_top_position = 10
-        base_usdt_money = 100
+        base_usdt_money = 50
 
         fig = go.Figure()
         oldest_crypto = self.find_oldest_crypto(symbol_list)
@@ -55,64 +56,78 @@ class BackTestSimulation:
 
         cloud_crypto_points = {}
 
+        # Collecting all days in dictionary
         for parent_day in oldest_crypto["days"]:  # FOR ALL BITCOIN DAYS
             day_turn += 1
-
             if day_turn > ending_day:
                 break
             if day_turn < starting_day:
                 continue
-
             if day_turn % multiple != 0:
                 continue
-
             for crypto in symbol_list:
                 day = self.getDayByDate(crypto, parent_day["date"])
                 if day is None:
                     continue
-
                 if day["top_position"] > max_top_position:
                     continue
-
-                self.storeCloudPoints(cloud_crypto_points, crypto, day_turn, day)
-
+                self.storeKeyToDict(cloud_crypto_points, day_turn, {day["name"]: day})
+                # self.storeCloudPoints(cloud_crypto_points, crypto, day_turn, day)
             print("day n°" + str(day_turn))
 
+        # Process backtesting
         daily_wallet_collection = {}
+        day_turn = -1
         for item in cloud_crypto_points.items():  # Loop all days
+            day_turn += 1
             x_list = []
             y_list = []
-            for day_entry in item[1].items():  # Loop all crypto present at this day
-                x_list.append(day_entry[0])
-                day = day_entry[1]
 
-                # if day["total_market_cap"] == -1:
-                #     continue
+            # step 1 : buy all with base money
+            # while the first day
+            if day_turn == 0:
+                for day_entry in item[1].items():  # Loop day of each crypto
+                    day = day_entry[1]
+                    if day["top_position"] >= max_top_position:
+                        percent_importance = 0
+                    else:
+                        percent_importance = day["market_cap"] / day["total_market_cap"] * 100
 
-                if day["top_position"] >= max_top_position:
-                    percent_importance = 0
-                else:
-                    percent_importance = day["market_cap"] / day["total_market_cap"] * 100
-                theoretical_invest = base_usdt_money * percent_importance / 100
+                    theoretical_invest = base_usdt_money * percent_importance / 100
+                    if not 0 <= theoretical_invest <= 100:
+                        assert False
 
-                if not 0 <= theoretical_invest <= 100:
-                    theoretical_invest # at somepoint, bitcoin is only 1.9% of the
-                #     total market cap, which means the T.M.C is way too big
+                    day.update({"percent_importance": percent_importance})
+                    day.update({"usdt_invest": theoretical_invest})
+                    day.update({"token_owned": theoretical_invest * day["price"]})  # add commission here
+                    self.storeKeyToDict(daily_wallet_collection, day["name"], day)
+                self.check_wallet_percent_is_equal_to_100(daily_wallet_collection)
 
+            # step 2 : sell and buy with left money
+            # while till the end
+            if day_turn > 0:
 
-                if day["name"] in daily_wallet_collection:
-                    daily_wallet_collection[day["name"]].append(theoretical_invest)
-                else:
-                    new_row = {day["name"]: []}
-                    daily_wallet_collection.update(new_row)
-                    daily_wallet_collection[day["name"]].append(theoretical_invest)
+                for day_entry in item[1].items():  # Loop day of each crypto
+                    day = day_entry[1]
+                    if day["top_position"] >= max_top_position:
+                        percent_importance = 0
+                    else:
+                        percent_importance = day["market_cap"] / day["total_market_cap"] * 100
 
-            # daily_wallet_collection.append(wallet_composition)
-            fig.add_trace(
-                trace=go.Scatter(x=x_list, y=y_list, mode='lines+markers', name=item[0]))
+                    theoretical_invest = base_usdt_money * percent_importance / 100
+                    if not 0 <= theoretical_invest <= 100:
+                        assert False
 
-        # fig.show()
-        self.check_wallet_percent_is_equal_to_100(daily_wallet_collection)
+                self.check_wallet_percent_is_equal_to_100(daily_wallet_collection)
+
+                fig.add_trace(
+                    trace=go.Scatter(x=x_list, y=y_list, mode='lines+markers', name=item[0]))
+
+    def sell_losers(self, day, base_usdt_money):  # returns earn usdt money
+        # sell x token too much compared to the percent_importance
+        pass
+
+    def buy_winners(self, day, base_usdt_money):  # returns
         pass
 
     def calculateTotalMarketCap(self, symbols, oldest_crypto, max_top_position, start_day, ending_day):
@@ -139,14 +154,11 @@ class BackTestSimulation:
             for day in presentAtThisDay:
                 day["total_market_cap"] = total_market_cap
 
-    def storeCloudPoints(self, cloud_crypto_points, crypto, day_turn, day):
-        new_point = {day_turn: day}
-        if crypto["name"] in cloud_crypto_points:
-            cloud_crypto_points[crypto["name"]].update(new_point)
+    def storeKeyToDict(self, dict_, key, item):
+        if key in dict_:
+            dict_[key].update(item)
         else:
-            new_crypto = {crypto["name"]: {}}
-            cloud_crypto_points.update(new_crypto)
-            cloud_crypto_points[crypto["name"]].update(new_point)
+            dict_.update({key: item})
 
     def getDayByDate(self, crypto, date):
         for day in crypto["days"]:
